dc5f5438a201656719765bff58942cbb81aae196
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-06-22 16:10:38 +0200

    Setup the client secret and id for the better auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,15 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/server/db"; // your drizzle instance
+import { env } from "@/env";
  
 export const auth = betterAuth({
+    socialProviders:{
+        google:{
+            clientId:env.GOOGLE_CLIENT_ID as string,
+            clientSecret:env.GOOGLE_CLIENT_SECRET as string
+        }
+    },
     database: drizzleAdapter(db, {
         provider: "pg",
     })
