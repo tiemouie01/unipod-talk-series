ba2be90342b9ab2590d2bcdb56712e896fc71879
Timothy Miamba <timal0361@gmail.com>
2025-06-24 09:45:36 +0200

    Add full name to logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Home } from "lucide-react";
 
 export function Header() {
   return (
@@ -7,15 +8,25 @@ export function Header() {
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-6 sm:px-6 lg:px-8">
         <div className="flex items-center space-x-6">
           <div className="flex items-center space-x-3">
-            <Image src="/logo.png" alt="UniPod" width={100} height={100} />
+            <div className="flex items-end">
+              <div className="mb-1">
+                <Image src="/logo.png" alt="UniPod" width={100} height={100} />
+              </div>
+              <div className="flex items-end">
+                <span className="text-3xl font-bold text-yellow-400">Talk</span>{" "}
+                <span className="text-3xl font-bold text-[#1d9ed9]">
+                  Series
+                </span>
+              </div>
+            </div>
           </div>
         </div>
         <nav className="flex space-x-8">
           <Link
             href="/"
-            className="text-lg font-semibold text-gray-800 transition-colors hover:text-[#1d9ed9]"
+            className="text-lg font-semibold text-[#1d9ed9] transition-colors"
           >
-            Home
+            <Home className="h-6 w-6" />
           </Link>
           {/* <Link
             href="/history"
