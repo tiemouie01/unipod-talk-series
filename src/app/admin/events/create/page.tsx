0367563584e5b4bf6cea4e4879035ee195563ff6
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-07-02 18:16:43 +0200

    RBAC admin dashboard

--- src/app/admin/events/create/page.tsx
+++ src/app/admin/events/create/page.tsx
@@ -1,6 +1,30 @@
 import { CreateEventForm } from "./_components/create-event-form";
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
+import { redirect } from "next/navigation"
+import { ErrorUI } from "@/components/ui/error"
 
-export default function CreateEventPage() {
+export default async function CreateEventPage() {
+  const userData = await auth.api.getSession({
+    headers: await headers()
+  })
+  const userId = userData?.session.userId  
+  const {success, error} = await auth.api.userHasPermission({
+    body:{
+      userId:userId,
+      permissions:{
+        user:["create"]
+      }
+    }
+  })
+  if(error){
+    return <div>
+      <ErrorUI title="An error occured while fetching user data" message={error}/>
+    </div>
+  }
+  if(!success){
+    redirect("/auth")
+  }
   return (
     <div className="min-h-[calc(100vh-3rem)]">
       <CreateEventForm />
--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,11 +2,14 @@ import type React from "react"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/app/admin/_components/admin-sidebar"
 
-export default function AdminLayout({
+export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  
+  
+
   return (
     <div>
       <div className="min-h-screen bg-gradient-to-br from-slate-900 via-blue-950 to-slate-900">
