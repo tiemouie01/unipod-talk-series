35f3eccc4b146e39af172a513c9505e1d36536b2
Timothy Miamba <timal0361@gmail.com>
2025-07-21 19:27:42 +0200

    Update background image

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -10,20 +10,19 @@ export default function MainLayout({
   return (
     <div className="grid h-dvh w-full" style={{ gridTemplateRows: "auto 1fr" }}>
       {/* Background image and overlay in grid cell 1/1, non-scrolling, spanning both rows */}
-      <div className="col-start-1 row-span-2 row-start-1 grid h-dvh w-full">
+      <div className="col-start-1 row-span-2 row-start-1 grid w-full">
         <Image
-          src="/unipod_banner.jpg"
+          src="/unipod-banner.webp"
           alt="Unipod Banner"
-          width={1000}
-          height={450}
+          fill
           className="col-start-1 row-start-1 h-full w-full object-cover"
         />
-        <div className="col-start-1 row-start-1 h-full w-full bg-gradient-to-br from-blue-900/80 via-slate-900/90 to-blue-950/80" />
+        <div className="z-10 col-start-1 row-start-1 h-full w-full bg-gradient-to-br from-blue-900/80 via-slate-900/90 to-blue-950/80" />
       </div>
       {/* Header in the first row */}
       <Header />
       {/* Content scrolls in the second row */}
-      <div className="col-start-1 row-start-2 overflow-auto">
+      <div className="z-20 col-start-1 row-start-2 overflow-auto">
         <div className="h-14" />
         <div>{children}</div>
       </div>
