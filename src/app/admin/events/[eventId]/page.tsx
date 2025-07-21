a4c6dca895eda47b5e9b542f42674582b5131ee8
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-07-21 15:53:37 +0200

    Format with prettier

--- src/app/admin/events/[eventId]/page.tsx
+++ src/app/admin/events/[eventId]/page.tsx
@@ -1,32 +1,37 @@
 import { EventDetail } from "./_components/event-detail";
-import { auth } from "@/lib/auth"
-import { headers } from "next/headers"
-import { redirect } from "next/navigation"
-import { ErrorUI } from "@/components/ui/error"
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { ErrorUI } from "@/components/ui/error";
 
 export default async function EventDetailPage(props: {
   params: Promise<{ eventId: string }>;
   searchParams: Promise<{ query: string }>;
 }) {
   const userData = await auth.api.getSession({
-    headers: await headers()
-  })
-  const userId = userData?.session.userId  
-  const {success, error} = await auth.api.userHasPermission({
-    body:{
-      userId:userId,
-      permissions:{
-        user:["create"]
-      }
-    }
-  })
-  if(error){
-    return <div>
-      <ErrorUI title="An error occured while fetching user data" message={error}/>
-    </div>
+    headers: await headers(),
+  });
+  const userId = userData?.session.userId;
+  const { success, error } = await auth.api.userHasPermission({
+    body: {
+      userId: userId,
+      permissions: {
+        user: ["create"],
+      },
+    },
+  });
+  if (error) {
+    return (
+      <div>
+        <ErrorUI
+          title="An error occured while fetching user data"
+          message={error}
+        />
+      </div>
+    );
   }
-  if(!success){
-    redirect("/")
+  if (!success) {
+    redirect("/");
   }
   const eventId = (await props.params).eventId;
   const query = (await props.searchParams).query;
